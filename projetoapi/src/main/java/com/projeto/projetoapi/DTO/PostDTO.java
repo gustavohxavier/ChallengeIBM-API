ee66c940cd6ebe0fccdc921c2376c329e3307506
Gustavo Hidalgo Xavier <gustavo.xavier@ibm.com>
2022-06-28 13:50:32 -0300

    Added Lombok dependencies and PostDTO attributes

---
+++ projetoapi/src/main/java/com/projeto/projetoapi/DTO/PostDTO.java
@@ -0,0 +1,18 @@
+package com.projeto.projetoapi.DTO;
+
+public class PostDTO {
+    private String nomeProduto;
+    private String nomeRegiao;
+    private String nomeUF;
+    private String mesEmissao;
+    private String anoEmissao;
+    private String cdPrograma;
+    private String cdSubPrograma;
+    private String cdFonteRecurso;
+    private String cdTipoSeguro;
+    private Integer qtdCusteio;
+    private Long vlCusteio;
+    private String atividade;
+    private String cdModalidade;
+    private Long areaCusteio;
+}
