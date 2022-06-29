2649ebe23aaed42243cb7074d237697833f0f529
Gustavo Hidalgo Xavier <gustavo.xavier@ibm.com>
2022-06-29 08:56:28 -0300

    Created ValueRepository class and implemented ValueResouce class

--- projetoapi/src/main/java/com/projeto/projetoapi/resources/ValueResouce.java
+++ projetoapi/src/main/java/com/projeto/projetoapi/resources/ValueResouce.java
@@ -1,4 +1,26 @@
 package com.projeto.projetoapi.resources;
 
+import com.projeto.projetoapi.entities.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/values")
 public class ValueResouce {
+
+    @GetMapping
+    public ResponseEntity<List<Value>> findAll(){
+        //TODO
+    }
+
+    //Consultar produtos por ID
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Value> findById(@PathVariable Long id){
+        //TODO
+    }
 }
