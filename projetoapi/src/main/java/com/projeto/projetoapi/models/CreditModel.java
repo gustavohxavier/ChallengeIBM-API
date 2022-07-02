70105f1f27a05d3b9226906fa86fd3ac3264f024
Gustavo Hidalgo Xavier <gustavo.xavier@ibm.com>
2022-07-02 15:16:07 -0300

    Bean problem

--- projetoapi/src/main/java/com/projeto/projetoapi/models/CreditModel.java
+++ projetoapi/src/main/java/com/projeto/projetoapi/models/CreditModel.java
@@ -1,5 +1,6 @@
 package com.projeto.projetoapi.models;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
 import javax.persistence.*;
@@ -20,15 +21,21 @@ public class CreditModel {
     private String nomeProduto;
     private String nomeRegiao;
     private String nomeUF;
+    @JsonProperty("MesEmissao")
     private String mesEmissao;
+    @JsonProperty("AnoEmissao")
     private String anoEmissao;
     private String cdPrograma;
     private String cdSubPrograma;
     private String cdFonteRecurso;
     private String cdTipoSeguro;
+    @JsonProperty("QtdCusteio")
     private Double qtdCusteio;
+    @JsonProperty("VlCusteio")
     private Double vlCusteio;
+    @JsonProperty("Atividade")
     private String atividade;
     private String cdModalidade;
+    @JsonProperty("AreaCusteio")
     private Double areaCusteio;
 }
